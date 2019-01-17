f2fb2127b20cb647f4d6de45578f8dc32c5e821c
Sruthi Krishna <sruthik926@gmail.com>
2019-01-17 16:37:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,7 @@
 # Add  code here!
 def prime?(int)
-<<<<<<< HEAD
   (2..(int - 1)).each do |x|.abs
     return false if int % x == 0 
-=======
-  2..(int - 1)).each do |x|
-    return false if int % x == 0
->>>>>>> 3e043137abbbedfeea67f05480a8dfe85bd22ae7
   end
    true
 end
